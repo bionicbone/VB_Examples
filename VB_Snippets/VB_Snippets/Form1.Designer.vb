012fad6371b03ecd7690efebb29128fe1cf8eee5
bionicbone <github@bionicbone.plus.com>
2014-11-20 20:10:21 +0000

    Progress Bar Code
    Added the Progress Bar Code as used in the APM Analyzer.

---
+++ VB_Snippets/VB_Snippets/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmProgressBar
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 116)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(235, 32)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(45, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(175, 24)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Under Windows 7"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(28, 33)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(208, 24)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Watch the Task Bar !!"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(89, 76)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'frmProgressBar
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(259, 160)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Name = "frmProgressBar"
+        Me.Text = "Progress Bar Example"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+End Class
---
+++ VB_Snippets/VB_Snippets/Form1.vb
@@ -0,0 +1,44 @@
+﻿Imports Microsoft.WindowsAPICodePack
+Imports Microsoft.WindowsAPICodePack.Taskbar
+
+
+Public Class frmProgressBar
+
+    ' *************************************************************************************
+    ' *** Progress Bar Solution for XP / Windows 7                                      ***
+    ' ***       Kevin Guest - 18/01/2013                                                ***
+    ' ***    Windows XP, 7 , and 8.1 Compatible                                         ***
+    ' ***                                                                               ***
+    ' *** Under Windows XP only the Form Progress Bar will be displayed.                ***
+    ' *** Under Windows 7 / 8 the Task Bar Icon will also be updated.                   ***
+    ' ***                                                                               ***
+    ' *** To import this example into local code you need to install the                ***
+    ' *** Windows API Code Pack into the Current VB Program.                            ***
+    ' ***   --- Tools Menu - NuGet Package Manager - Manage NuGet Packages for Solution ***
+    ' ***   --- Install the Windows 7 API Code Pack.                                    ***
+    ' *************************************************************************************
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'Declare the Variables
+        Dim osVer As Version = Environment.OSVersion.Version                            'Detect Windows Environment.
+        Dim frmMainFormHandle As Double = Me.Handle()                                   ' The Handle of the main form.
+
+        For N = 1 To 100
+            ProgressBar1.Value = N
+
+            If osVer.Major >= 6 And osVer.Minor >= 1 Then                               'Only allowed in Windows 7 or above
+                TaskbarManager.Instance.SetProgressValue(N, 100, frmMainFormHandle)
+            End If
+
+            Application.DoEvents()
+            Threading.Thread.Sleep(100)
+        Next N
+
+        ' Reset the Task Bar otherwise it will stay green.
+        If osVer.Major >= 6 And osVer.Minor >= 1 Then                               'Only allowed in Windows 7 or above
+            TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Normal, frmMainFormHandle)
+        End If
+
+    End Sub
+End Class
