8f18d7ed9180c729c398af7ee200407764d39b4e
bionicbone <github@bionicbone.plus.com>
2014-11-24 07:09:39 +0000

    SQL Update
    Add an SQL Update Example

---
+++ VB_Snippets/VB_Snippets/SQL Update Example.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Data.SqlClient
+
+Module SQL_Update_Example
+
+    Dim xData As SqlDataReader
+    Dim mySqlConnection As New SqlConnection
+    Dim mysqlCommand As SqlCommand
+
+    Public Sub SQL_Update()
+
+        ' *************************************************************************************
+        ' *** SQL Update Example                                                              ***
+        ' ***       Kevin Guest - Nov 2014                                                  ***
+        ' ***    Windows XP, 7 , and 8.1 Compatible                                         ***
+        ' ***                                                                               ***
+        ' *** To test this example:                                                         ***
+        ' *** --- Create a Database called HobbyKingDB.                                     ***
+        ' *** --- Within that BD there must be a Table called Table_1.                      ***
+        ' *** --- Within that table create 3 fields called Field_0, Field_1 and Field_2.    ***
+        ' ***                                                                               ***
+        ' *************************************************************************************
+
+        ' NOTE: The correct connection string can be found by opening the SQL Server Object Explorer
+        ' from with in Visual Studio 2013.
+        ' View --- SQL Server Object Explorer
+        ' Select the Database you want to connect to and select properties.
+        ' Find the property "Connection String" and use that here. 
+
+        ' Connect to the DB.
+        mySqlConnection.ConnectionString = _
+            "Data Source=LT5-PC\SQLEXPRESS;Initial Catalog=HobbyKingDB;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False"
+        mySqlConnection.Open()
+
+        ' Set up some Variables to write.
+        Dim Data_Name As String = "John"
+        Dim Data_Age As String = "87"
+        Dim Data_Month As String = "June"
+
+        ' Create the UPDATE Statement.
+        mysqlCommand = New SqlCommand(("UPDATE Table_1 " & _
+                                       "SET Field_1 = '" & Data_Age + 1 & "', " & _
+                                       "Field_2 = '" & Data_Month & "' " & _
+                                       "WHERE (Field_0 = '" & Data_Name & "')"), mySqlConnection)
+        mysqlCommand.ExecuteNonQuery()
+
+        ' Alway Close the Connection after use or when terminating the program.
+        mySqlConnection.Close()
+
+    End Sub
+
+End Module
--- VB_Snippets/VB_Snippets/frmMainSelectionForm.Designer.vb
+++ VB_Snippets/VB_Snippets/frmMainSelectionForm.Designer.vb
@@ -27,6 +27,7 @@ Partial Class frmMainSelectionForm
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -50,7 +51,7 @@ Partial Class frmMainSelectionForm
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(26, 92)
+        Me.Button2.Location = New System.Drawing.Point(26, 121)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(84, 23)
         Me.Button2.TabIndex = 2
@@ -68,18 +69,28 @@ Partial Class frmMainSelectionForm
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(26, 121)
+        Me.Button3.Location = New System.Drawing.Point(26, 92)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(84, 23)
         Me.Button3.TabIndex = 4
         Me.Button3.Text = "SQL Write"
         Me.Button3.UseVisualStyleBackColor = True
         '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(26, 150)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(84, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "SQL Update"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
         'frmMainSelectionForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(591, 262)
+        Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button2)
@@ -96,4 +107,5 @@ Partial Class frmMainSelectionForm
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
 End Class
