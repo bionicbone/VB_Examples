0b629eda32ce91571a06c50b5149771b99ae15a6
bionicbone <github@bionicbone.plus.com>
2014-11-29 06:02:35 +0000

    StopWatch and CPU Examples
    Added and stopwatch and CPU % example

---
+++ VB_Snippets/VB_Snippets/CodeTester.vb
@@ -0,0 +1,33 @@
+﻿'Imports System.Diagnostics
+
+Module CodeTester
+
+    Sub TestThis()
+        Dim sWatch As System.Diagnostics.Stopwatch
+
+        sWatch = New System.Diagnostics.Stopwatch()
+
+        sWatch.Start()
+
+        For N = 1 To 10000
+
+
+            'If N / 100 - Int(N / 100) = 0 Then
+            '    Debug.Print(N)
+            'End If
+
+
+            If N Mod 100 = 0 Then
+                Debug.Print(N)
+            End If
+
+        Next
+
+        sWatch.Stop()
+
+        Debug.Print(sWatch.Elapsed.ToString)
+
+
+    End Sub
+
+End Module
--- VB_Snippets/VB_Snippets/frmMainSelectionForm.Designer.vb
+++ VB_Snippets/VB_Snippets/frmMainSelectionForm.Designer.vb
@@ -28,6 +28,9 @@ Partial Class frmMainSelectionForm
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -85,11 +88,41 @@ Partial Class frmMainSelectionForm
         Me.Button4.Text = "SQL Update"
         Me.Button4.UseVisualStyleBackColor = True
         '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(504, 63)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "Test This"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(122, 63)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(84, 23)
+        Me.Button6.TabIndex = 7
+        Me.Button6.Text = "StopWatch"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(122, 92)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(84, 23)
+        Me.Button7.TabIndex = 8
+        Me.Button7.Text = "CPU %"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
         'frmMainSelectionForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(591, 262)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Label2)
@@ -108,4 +141,7 @@ Partial Class frmMainSelectionForm
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Button3 As System.Windows.Forms.Button
     Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
 End Class
