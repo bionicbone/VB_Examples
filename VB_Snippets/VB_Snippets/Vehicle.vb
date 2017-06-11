eebca9f36fdf33c54b9b94fb692398f50056a847
Kevin Guest <PublicGitHub@bionicbone.plus.com>
2017-06-11 14:43:26 +0100

    Classes
    More indepth look at classes.
    Adds an abstract class called vehicle that mustinherit
    Add a class called car and anther called truck that inherits vehicle
    Add Overrides methods in both car and truck classes.

---
+++ VB_Snippets/VB_Snippets/Vehicle.vb
@@ -0,0 +1,13 @@
+﻿Public MustInherit Class Vehicle
+    ' NOTE: This is an Abstract Class that has not methods
+    '       It can only be used by inheriting these properties.
+    '       into the Car or Truck Class
+
+    Public Property Make As String
+    Public Property Model As String
+    Public Property Year As Integer
+    Public Property Colour As String
+
+    Public MustOverride Function FormatMe() As String
+
+End Class
